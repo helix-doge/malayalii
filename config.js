b8929051da37fb9a05c2099734c96abfa8fd89a6
helix-doge <helixnew0201@gmail.com>
2026-09-12 17:02:32 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+// Supabase Client Initialization
+const SUPABASE_URL = "https://lkamcxruiqwqcgkfkfca.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_OeygUfbL1ykCKLOIj9VCbA_h0y4H77N";
+
+const db = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+// Admin Credentials
+const ADMIN_ID = "mallu@123";
+const ADMIN_PASS = "malayali@0201";
